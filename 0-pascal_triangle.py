c2306c573a6167807e19f557afdc70bd471910d4
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-10-02 09:40:05 -0700

    task 0

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""my attempt at pascals triangle
+"""
+
+from math import factorial
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for k in range(n):
+        row = []
+        for c in range(k + 1):
+            ncr = factorial(k) // (factorial(c) * factorial(k - c))
+            row.append(ncr)
+        triangle.append(row)
+    
+    return triangle
