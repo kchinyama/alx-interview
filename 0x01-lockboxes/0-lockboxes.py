f4fdeefa476c8a0ca0617d4917f3a0d174e98970
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-10-12 05:27:09 -0700

    task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""demo of code that checks to see if any lock can be unlocked
+"""
+
+
+def canUnlockAll(boxes):
+    """ Method that determines if all boxes can be opened """
+
+    for key in range(1, len(boxes)):
+        # create loop to iterate through all the boxes
+
+        flag = False # variable that checks state of the lock
+
+        for box in range(len(boxes)):
+            # for each box in the loop check if the key opens it
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+This directory contains my attempt at the alx locked box project task
